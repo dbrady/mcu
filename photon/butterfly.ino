83e044aab0e6c526f3bc61272d4f7ec869a83fab
David Brady <dbrady@covermymeds.com>
2016-11-08 20:43:31 -0700

    Clean up butterfly

--- photon/butterfly.ino
+++ photon/butterfly.ino
@@ -1,11 +1,23 @@
 // butterfly.ino
-
+//
 // Result of an evening screwing around with Photon. I'm driving a funky 6-digit
-// 14-segment LED display with it. The Photon has 18 GPIO pins; 15 pins send +V
-// to a given LED segment (14 segments + 1 for the decimal point). That leaves 3
-// pins for selecting the digit... not to worry! Enter the 74LS138, a 3-to-8
-// binary decoder. Give it three pins and it will give you 8 in return: whatever
-// binary number you encode, it will select that pin.
+// 14-segment LED display with it, and rather than make it spell a rude word
+// because I'm twelve (not saying I'm better than that; I've already done it and
+// moved on). This project animates a little butterfly flapping across the
+// display.
+//
+// The LED is common anode; you put +V to one of the segments (the rows) and
+// ground one of the digits (the columns) to light up one segment in one
+// digit. The 74LS138 is cleverly designed to be "active low", so no extra parts
+// are needed to make it work. Ground pin A and leave pins B and C high, and it
+// will ground digit 1 of the display. Ground pins A and C, and it will ground
+// digit 5 of the display. Nifty.
+//
+// The Photon has 18 GPIO pins; 15 pins send +V to a given LED segment (14
+// segments + 1 for the decimal point). That leaves 3 pins for selecting the
+// digit... not to worry! Enter the 74LS138, a 3-to-8 binary decoder. Give it
+// three pins and it will give you 8 in return: whatever binary number you
+// encode, it will select that pin.
 //
 // One drawback to this design is that all of my pins are in use! If I wanted to
 // make a sensing/display project, I'd need to sacrifice the decimal point to
@@ -27,10 +39,37 @@
 //    f |  h \   j |   k /    | b
 //      |     \    |    /     |
 //      |      \   |   /      |
-//      |       \  |  /       |
+//      |  g1   \  |  /   g2  |
+//       ---------- ----------
+//      |  g1   /  |  \   g2  |
+//      |      /   |   \      |
+//      |     /    |    \     |
+//    f |  n /   m |   l \    | b
+//      |   /      |      \   |
+//      |  /       |       \  |
+//      | /        |        \ |
+//       ---------------------
+//                 d
 //
-// D0
+// D0 -> A
+// D1 -> B
+// D2 -> C
+// D3 -> D
+// D4 -> E
+// D5 -> F
+// D6 -> G1
+// D7 -> G2
+// A0 -> H
+// A1 -> J
+// A2 -> K
+// A3 -> L
+// A4 -> M
+// A5 -> N
 //
+// WKP -> DIGIT_SELECT_A
+// RX  -> DIGIT_SELECT_B
+// TX  -> DIGIT_SELECT_C
+
 
 int SEGMENTS[] = { D0, D1, D2, D3, D4, D5, D6, D7, /* A-G2 */
                    A0, A1, A2, A3, A4, A5, A6 };
@@ -49,10 +88,6 @@ int ON=HIGH; // power on a segment to turn it on...
 int OFF=LOW;
 int DIGIT_ON=OFF; // ...and ground the desired digit to complete the circuit
 int DIGIT_OFF=ON;
-
-int display[] = { 36, 47, 87, 109, 125, 126 };
-
-
 
 void clear(void) {
   for(int i=0; i<NUM_SEGMENTS; i++) {
@@ -75,10 +110,12 @@ void writeDigit(uint16_t digit) {
   }
 }
 
+// tell me you're alive, baby
 void winkOn(void) {
   digitalWrite(D7, HIGH);
 }
 
+// okay baby, go back to playing dead
 void winkOff(void) {
   digitalWrite(D7, LOW);
 }
@@ -95,8 +132,8 @@ void setup() {
     digitalWrite(SEGMENTS[i], OFF);
   }
 
-  // get some attention!
-  for(int i=0; i<4; i++) {
+  // did we even boot?!? Get some attention!
+  for(int i=0; i<2; i++) {
     for(int j=0; j<NUM_SEGMENTS; j++) {
       digitalWrite(SEGMENTS[j], ON);
     }
@@ -104,25 +141,33 @@ void setup() {
       selectDigit(j+1);
       delay(25);
     }
+    for(int j=NUM_DIGITS; j>0; j--) {
+      selectDigit(j+1);
+      delay(25);
+    }
   }
   clear();
+  delay(200);
+  for(int i=0; i<200; i++) {
+    for(int j=0; j<NUM_SEGMENTS; j++) {
+      digitalWrite(SEGMENTS[j], ON);
+    }
+    for(int j=0; j<NUM_DIGITS; j++) {
+      selectDigit(j+1);
+      delay(1);
+    }
+  }
+  clear();
+  delay(200);
 }
-
-void waggle(void) {
-  digitalWrite(TX, LOW);
-  delay(50);
-  digitalWrite(TX, HIGH);
-  delay(50);
-}
-
 
 uint16_t butterfly[] = {
-  // frame 1:  \ /
-  //           / \
+  // frame 1:  \ / .
+  //           / \ .
   0b010110100000000,
 
-  // frame 2: \|
-  //          /|
+  // frame 2: \|  .
+  //          /|  .
   0b011001100000000,
 };
 
